a2cc5187a4419c31e24096dbf6eb293cecd54dc1
bservent <bservent@gmail.com>
2022-04-01 11:48:54 -0700

    initial programming conconcepts

---
+++ programming_101.js
@@ -0,0 +1,11 @@
+/* INTRO TO THE CORE CONCEPTS OF PROGRAMMING */
+
+/* IDE = intergrated development environment or text editor
+
+syntax = way in which a language communicates that consists of rules and vocabulary
+
+variables = stored info that you can pass on and manipulate later
+
+printing = runs program on screen (backend lanauges need this)
+
+*/
