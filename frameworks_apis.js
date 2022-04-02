3803be73f7be8f15d89f27f820faca6dabf989dc
bservent <bservent@gmail.com>
2022-04-02 10:23:58 -0700

    frameworks vs.libraries

---
+++ frameworks_apis.js
@@ -0,0 +1,25 @@
+/* FRAMEWORKS AND APIS */
+
+/* What is a framework? */
+
+/* framework = premade architexture = code that makes things easier (has specific file structure and connects the files)
+frameworks take out redundency (lots of blog sites, so most of the code for a blog site has already been done) this makes things faster and easier (more time can be spent on the things that are different) */
+
+/* IDE */
+
+/* ide = integrated development environment = program ("microsoft word") that programmers write their code in (has special features to create code to make it easier and alos has contains develoment environment where you can test code) */
+
+
+/* Library */
+
+/* library = premade code chunks in which you determine the flow of the code (unlike frameworks)
+
+A library is like going to Ikea. You already have a home, but you need a bit of help with furniture. You don’t feel like making your own table from scratch. Ikea allows you to pick and choose different things to go in your home. You are in control.
+
+A framework, on the other hand, is like building a model home. You have a set of blueprints and a few limited choices when it comes to architecture and design. Ultimately, the contractor and blueprint are in control. And they will let you know when and where you can provide your input.
+*/
+
+/* API */
+
+/* api = mini app that is built on top of applicaiton that handles the transfer of data (like a waiter at a restuarant taking an order) 
+*/
